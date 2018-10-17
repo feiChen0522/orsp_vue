ff78779ef83209cbc357220755e36fcc5e43e250
starryrbs <35923714+starryrbs@users.noreply.github.com>
2018-10-17 08:33:21 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import Regist from './components/Regist'
 import Nav from './components/Nav'
 import CenterNav from './components/CenterNav'
 import Main from './components/Main'
-<<<<<<< HEAD
+
 import Swing from './components/Swing'
 import HelloWorld from './components/HelloWorld'
-=======
+
 import Header from './components/Header'
 import Swing from './components/Swing'
->>>>>>> f4a98e73678cd8d0566ca2537691be62343db6ef
+
 import Goods from './components/Goods'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false;
@@ -23,10 +23,7 @@ Vue.component('Regist-div',Regist);
 Vue.component('nav-div',Nav);
 Vue.component('center-nav',CenterNav);
 Vue.component('main-nav',Main);
-<<<<<<< HEAD
-=======
 Vue.component('header-top',Header);
->>>>>>> f4a98e73678cd8d0566ca2537691be62343db6ef
 Vue.component('swing-div',Swing);
 Vue.component('goods-div',Goods);
 new Vue({
