4a1814d4c599627ca181b25eee6ee1e884d46876
starryrbs <1322096624@qq.com>
2018-11-07 15:59:49 +0800

    rbs 最后一次提交

--- src/Global.vue
+++ src/Global.vue
@@ -7,6 +7,7 @@
   // const serverPath="http://123.207.11.101:8000";
   // const serverPath=this.global.serverPath+'";
   const serverPath="http://120.77.153.223:8000";
+  // const serverPath="http://127.0.0.1:8000";
 
   export default{
     serverPath
